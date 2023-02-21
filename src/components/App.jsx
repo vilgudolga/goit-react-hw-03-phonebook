64a82058e7fc76992ba778872078498a6e6f9178
Olga <vilgudolga@gmail.com>
2023-02-21 20:37:57 +0100

    HW-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,81 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import Notiflix from 'notiflix';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = ({ id, name, number }) => {
+    const { contacts } = this.state;
+    if (
+      contacts.find(
+        contacts => contacts.name.toLowerCase() === name.toLowerCase()
+      )
+    )
+      return Notiflix.Notify.failure(`${name} is already in phonebook`);
+
+    this.setState(prevState => {
+      return { contacts: [...prevState.contacts, { id, name, number }] };
+    });
+  };
+
+  removeContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  getContacts = () => {
+    const { filter, contacts } = this.state;
+    const filterNormalize = filter.toLowerCase();
+    return contacts.filter(contacts =>
+      contacts.name.toLowerCase().includes(filterNormalize)
+    );
+  };
+
+  filterContacts = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <>
+        <div>
+          <h1>Phonebook</h1>
+          <ContactForm onSubmitData={this.addContact} />
+
+          <h2>Contacts</h2>
+          <Filter value={filter} onChange={this.filterContacts} />
+          <ContactList
+            contacts={this.getContacts()}
+            handleRemove={this.removeContact}
+          />
+        </div>
+      </>
+    );
+  }
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+import ContactListItem from 'components/ContactListItem/ContactListItem';
+import PropTypes from 'prop-types';
+import styles from './ContactList.module.css';
+
+class ContactList extends Component {
+  render() {
+    const { contacts, handleRemove } = this.props;
+    return (
+      <ul className={styles.list}>
+        {contacts.length
+          ? contacts.map(({ id, name, number }) => (
+              <ContactListItem
+                key={id}
+                id={id}
+                name={name}
+                number={number}
+                handleRemove={handleRemove}
+              />
+            ))
+          : 'No contacts'}
+      </ul>
+    );
+  }
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  handleRemove: PropTypes.func,
+};
+
+export default ContactList;
